662bc1c12ac5c63ddb0ae295178357d72f279488
Aneri <aneri.au@gmail.com>
2026-05-30 14:56:14 +1000

    creating the COVID section of the visualisation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const charts = [
     {id: "#flow_map", spec: "vega/flow_map_outbound.vg.json"},
-    {id: "#aud_scatter", spec: "vega/aud_scatter.vg.json"}
+    {id: "#aud_scatter", spec: "vega/aud_scatter.vg.json"},
+    {id: "#recovery_slope", spec: "vega/covid_slope.vg.json"},
+    {id: "#recovery_map", spec: "vega/covid_recovery_map.vg.json"},
+    {id: "#recovery_bars", spec: "vega/covid_recovery.vg.json"}
 ];
 
 charts.forEach(({id, spec}) => {
