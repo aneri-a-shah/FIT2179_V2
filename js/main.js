25caa823119f584c6a9906cb6f0009bd89ad5be5
Aneri <aneri.au@gmail.com>
2026-05-30 21:52:10 +1000

    creating the two australia maps for regional

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const charts = [
     {id: "#flow_map", spec: "vega/flow_map_outbound.vg.json"},
     {id: "#aud_scatter", spec: "vega/aud_scatter.vg.json"},
     {id: "#recovery_slope", spec: "vega/covid_slope.vg.json"},
-    {id: "#recovery_bars", spec: "vega/covid_recovery.vg.json"}
+    {id: "#recovery_bars", spec: "vega/covid_recovery.vg.json"},
+    {id: "#domestic_dots", spec: "vega/domestic_dots.vg.json"},
+    {id: "#domestic_choro", spec: "vega/domestic_choro.vg.json"}
 ];
 
 charts.forEach(({id, spec}) => {
