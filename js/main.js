7c9f1cc063a7ee0cecae1d6f09eaeb98325d8efb
Aneri <aneri.au@gmail.com>
2026-05-30 19:03:34 +1000

    graph changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const charts = [
     {id: "#flow_map", spec: "vega/flow_map_outbound.vg.json"},
     {id: "#aud_scatter", spec: "vega/aud_scatter.vg.json"},
     {id: "#recovery_slope", spec: "vega/covid_slope.vg.json"},
-    {id: "#recovery_map", spec: "vega/covid_recovery_map.vg.json"},
     {id: "#recovery_bars", spec: "vega/covid_recovery.vg.json"}
 ];
 
