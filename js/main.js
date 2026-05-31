db1f7f94b4b06bc14ea5d384e5530a2a5939a64e
Aneri <aneri.au@gmail.com>
2026-05-31 16:30:34 +1000

    budget breakdown section data and graphs

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ const charts = [
     {id: "#state_multiples", spec: "vega/state_small_multiples.vg.json"},
     {id: "#state_combined", spec: "vega/state_combined.vg.json"},
     {id: "#daytrip_lines", spec: "vega/daytrip_lines.vg.json"},
-    {id: "#trip_spend", spec: "vega/trip_spend.vg.json"}
+    {id: "#trip_spend", spec: "vega/trip_spend.vg.json"},
+    {id: "#budget_donut", spec: "vega/budget_donut.vg.json"},
+    {id: "#budget_heatmap", spec: "vega/budget_heatmap.vg.json"}
 ];
 
 charts.forEach(({id, spec}) => {
